2783c121a4c7d5ad896fe38fa002ec6bce2f79f1
Fernando Mirasol <fhermq@gmail.com>
2025-10-30 16:06:58 -0600

    Connection with microservices product

--- businessdomain/customers/src/main/java/com/pixan/customer/repository/CustomerRepository.java
+++ businessdomain/customers/src/main/java/com/pixan/customer/repository/CustomerRepository.java
@@ -1,14 +1,22 @@
 package com.pixan.customer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pixan.customer.entities.Customer;
+
 /**
  * @author Fernando
  * 
  */
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	
+	@Query("SELECT c FROM Customer c WHERE c.code =?1")
+	public Customer findByCode(String code);
+	
+	@Query("SELECT c FROM Customer c WHERE c.iban =?1")
+	public Customer findByAccount(String iban);
 	
 
 }
